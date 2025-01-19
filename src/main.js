97f6f6869c9d4ba18c6fa3eab3070d501189252d
unknown <viralbhoot55@gmail.com>
2025-01-19 13:05:17 +0530

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-window.$ = require('jquery')
-window.JQuery = require('jquery')
-Vue.config.productionTip = false
-
-new Vue({
-    render: h => h(App),
-}).$mount('#app')
+version https://git-lfs.github.com/spec/v1
+oid sha256:48c8ed69500d402fb19bf278d1e4a5b307d6accf9693be2611f310f40779e5ed
+size 200
